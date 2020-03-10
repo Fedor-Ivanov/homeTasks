e963321d9aa14da4dc5e9e26931aa90ee311d622
FEDOR IVANOV <fedorhim@gmail.com>
2020-03-10 22:32:59 +0200

    start task7

---
+++ task7/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <p>redux</p>
+      <p>react-redux</p>
+      <p>react-router-dom</p>
+      <p>axios</p>
+    </div>
+  )
+}
+
+export default App
